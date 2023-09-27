0c276f68238ed55424475e6fac94c2711c14e03b
Hannes Drescher <hannescher9@gmail.com>
2023-09-27 16:24:33 +0200

    Remove unused routes

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,14 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\GrantController;
 use App\Http\Controllers\UserController;
-use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', fn() => view('home'))->name('home');
-    Route::resource('users', UserController::class);
-    Route::resource('companies', CompanyController::class);
-    Route::resource('categories', CategoryController::class);
-    Route::resource('companies.contacts', Contact::class)->shallow();
+    Route::resource('users', UserController::class)->except(['show', 'create']);
+    Route::resource('companies', CompanyController::class)->except(['create']);
+    Route::resource('categories', CategoryController::class)->except(['show', 'create']);
+    // Route::resource('companies.contacts', ContactController::class)->shallow();
     Route::resource('companies.grants', GrantController::class)->shallow();
     Route::post('/companies/{company}/grants/import', [GrantController::class, 'import'])->name('companies.grants.import');
     Route::get('/grants', [GrantController::class, 'index'])->name('grants.index');
